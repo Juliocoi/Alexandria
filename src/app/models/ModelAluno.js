aad61ca917dfa94a2101f275f9ff35e6b442bf34
Juliocoi <amorim.jc@gmail.com>
2022-07-25 19:00:04 -0300

    inclusão de filiação ao ModelAluno.js

--- src/app/models/ModelAluno.js
+++ src/app/models/ModelAluno.js
@@ -1,5 +1,7 @@
-function ModelAluno(nome, email, matricula, dataDeNascimento) {
+function ModelAluno(nome, mae, pai email, matricula, dataDeNascimento) {
     this.nome = nome;
+    this.mae = mae;
+    this.pai = pai;
     this.email = email;
     this.matricula = matricula;
     this.dataDeNascimento = dataDeNascimento;
