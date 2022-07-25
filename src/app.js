5476d8c5d7a5a66ac26e4ad5f835766f7969959b
hanakathy <khanamartins@gmail.com>
2022-07-25 15:29:20 -0400

    adicionou app.js e server.js ao projeto

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
+const express = require('express');
+const routes = require('./routes.js');
 
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+module.exports = new App().server;
