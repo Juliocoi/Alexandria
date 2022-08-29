2b85da9e03a79a8f15df147f8b74d2a599f89c19
hanakathy <khanamartins@gmail.com>
2022-08-29 17:17:37 -0400

    acrescentado foreign key no ModelAluno

--- src/app/models/usuario/ModelAluno.js
+++ src/app/models/usuario/ModelAluno.js
@@ -2,16 +2,24 @@ const ModelUsuario = require("./ModelUsuario");
 import Sequelize from "sequelize";
 
 class ModelAluno extends ModelUsuario () {
-    static init(sequelize){
-        super.init(
-            {
-                matricula: Sequelize.INTEGER,
+	static init(sequelize){
+		super.init(
+		    {
+                id: 
+                {
+                    type: Sequelize.UUIDV4(),
+                    references: {
+                        model: Usuario,
+                        key: 'id',
+                        }
+                },
+                matricula: Sequelize.INTEGER
             },
             {
                 sequelize,
             }
-        );
-    }
+	    );
+	}
 }
 
 export default ModelAluno;
