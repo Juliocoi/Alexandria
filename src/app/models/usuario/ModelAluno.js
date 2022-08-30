dce2019225fb6fb6ea659e692c47e459974f98dd
hanakathy <khanamartins@gmail.com>
2022-08-29 21:02:04 -0400

    foreign keys adicionadas corretamente nas models Usuario, Aluno, Funcionario

--- src/app/models/usuario/ModelAluno.js
+++ src/app/models/usuario/ModelAluno.js
@@ -1,25 +1,32 @@
 const ModelUsuario = require("./ModelUsuario");
 import Sequelize from "sequelize";
+const database = require('../../../config/database');
 
-class ModelAluno extends ModelUsuario () {
-	static init(sequelize){
-		super.init(
-		    {
-                id: 
-                {
-                    type: Sequelize.UUIDV4(),
-                    references: {
-                        model: Usuario,
-                        key: 'id',
-                        }
-                },
-                matricula: Sequelize.INTEGER
-            },
-            {
-                sequelize,
-            }
-	    );
+const sequelize = new Sequelize(database);
+
+class ModelAluno extends ModelUsuario {}
+ModelAluno.init(
+	{
+		id: Sequelize.UUIDV4(),
+		matricula: Sequelize.INTEGER,
+		usuarioId:{
+			type: Sequelize.UUIDV4(), //o bd ainda não sabe como gerar esse uuid
+			references: {
+				model: Usuario,
+				key: 'id',
+				}
+		},
+	},
+	{
+		sequelize,
+		modelName: "alunos",
+		timestamps: false,
 	}
-}
+);
+
+//aqui faz associação 1:n
+ModelAluno.hasOne(ModelUsuario,{
+    foreignKey: "usuarioId"
+});
 
 export default ModelAluno;
--- src/app/models/usuario/ModelUsuario.js
+++ src/app/models/usuario/ModelUsuario.js
@@ -1,22 +1,30 @@
-import Sequelize, { Model } from "sequelize";
+const ModelUsuario = require("./ModelUsuario");
+import Sequelize from "sequelize";
+import ModelAluno from "./ModelAluno";
+import ModelFuncionario from "./ModelFuncionario";
 const database = require('../../../config/database');
 
-class ModelUsuario extends Model{
-    static init(sequelize){
-        super.init(
-            {
-                id: Sequelize.UUIDV4(),
-                nome: Sequelize.STRING,
-                email: Sequelize.STRING,
-                dataDeNascimento: Sequelize.DATE,
-                mae: Sequelize.STRING,
-                pai: Sequelize.STRING,
-            },
-            {
-                sequelize,
-            }
-        );
+const sequelize = new Sequelize(database);
+
+class ModelUsuario extends Model{}
+
+ModelUsuario.init(
+    {
+        id: Sequelize.UUIDV4(),
+        nome: Sequelize.STRING,
+        email: Sequelize.STRING,
+        dataDeNascimento: Sequelize.DATE,
+        mae: Sequelize.STRING,
+        pai: Sequelize.STRING,
+    },
+    {
+        sequelize,
+        modelName: "usuarios", //colocar o nome como tá na migrations
+        timestamps: false,
     }
-}
+);  
+
+ModelUsuario.belongsTo(ModelAluno);
+ModelUsuario.belongsTo(ModelFuncionario);
 
 export default ModelUsuario;
