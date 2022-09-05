23883f79905d2c3f45f3237d28ef1a4f7269ef0e
Juliocoi <amorim.jc@gmail.com>
2022-09-04 21:53:45 -0300

    Add simples comentário para orientação interna

--- src/app/models/usuario/ModelFuncionario.js
+++ src/app/models/usuario/ModelFuncionario.js
@@ -21,7 +21,7 @@ ModelFuncionario.init(
     {
         sequelize,
         modelName: "funcionarios",
-        timestamps: false,
+        timestamps: false, // indica que o banco não terá a coluna de registros(tipo um log para as alterações. Esta coluna é para controle do interno do banco de dados), caso omisso, teremos que criar uma coluna para guardar essas informações (createdAt, updatedAt, deactivatedAt). Essas colunas devem ser add apenas nas migration. Não há necesside de colocá-las na model.
     }
 );
 
--- src/app/models/usuario/ModelUsuario.js
+++ src/app/models/usuario/ModelUsuario.js
@@ -19,7 +19,7 @@ ModelUsuario.init(
     {
         sequelize,
         modelName: "usuarios", //colocar o nome como tá na migrations
-        timestamps: false,
+        timestamps: false, // indica que o banco não terá a coluna de registros(tipo um log para as alterações. Esta coluna é para controle do interno do banco de dados), caso omisso, teremos que criar uma coluna para guardar essas informações (createdAt, updatedAt, deactivatedAt). Essas colunas devem ser add apenas nas migration. Não há necesside de colocá-las na model.
     }
 );  
 
