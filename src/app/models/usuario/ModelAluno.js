a38764c23707b5a7709738ab446f13e5874d401a
Juliocoi <amorim.jc@gmail.com>
2022-09-11 21:57:42 -0300

    Altera os models aluno e funcionários seguindo os padrões do novo banco

--- src/app/models/usuario/ModelAluno.js
+++ src/app/models/usuario/ModelAluno.js
@@ -1,40 +1,48 @@
-import ModelUsuario from "./ModelUsuario";
 import { Sequelize, DataTypes, Model } from "sequelize";
 import database from "../../../config/database";
 
 const sequelize = new Sequelize(database);
 
-class ModelAluno extends Model {} //refatoração pro extends do Model
+class ModelAluno extends Model {};
 
 ModelAluno.init(
 	{
-		id: {
-			type: DataTypes.UUID,
-			defaultValue: DataTypes.UUIDV4,
+		id:{
+			type: DataTypes.UUIDV4,
 			primaryKey: true
 		},
-				
-		usuarioId:{
-			type: DataTypes.UUID,
-			defaultValue: DataTypes.UUIDV4,
-			references: {
-				model: 'usuarios',
-				key: 'id',
-			}
+		matricula:{
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			unique: true
 		},
+		nome:{
+			type: DataTypes.STRING(100),
+			allowNull: false,
+		},
+		email:{
+			type: DataTypes.STRING(45),
+			allowNull: false,
+		},
+		dataDeNascimento:{
+			type: DataTypes.DATE,
+			allowNull: false
+		},
+		mae:{
+			type: DataTypes.STRING(100),
+			allowNull: false
+		},
+		pai:{
+			type: DataTypes.STRING(100),
+			allowNull: true
+		}
 	},
 	{
 		sequelize,
-		tableName: "alunos",
-		timestamps: false, // indica que o banco não terá a coluna de registros(tipo um log para as alterações. Esta coluna é para controle do interno do banco de dados), caso omisso, teremos que criar uma coluna para guardar essas informações (createdAt, updatedAt, deactivatedAt). Essas colunas devem ser add apenas nas migration. Não há necesside de colocá-las na model.
+		modelName: "ModelAluno",
+		timestamps: true, // indica que o banco terá a coluna de registros(tipo um log para as alterações. Esta coluna é para controle do interno do banco de dados), caso omisso, teremos que criar uma coluna para guardar essas informações (createdAt, updatedAt, deactivatedAt). Essas colunas devem ser add apenas nas migration. Não há necesside de colocá-las na model.
+		tableName: 'Alunos'
 	}
 );
-
-//aqui faz associação 1:n
-ModelAluno.belongsTo(ModelUsuario)
-
-ModelUsuario.hasMany(ModelAluno, {
-    foreignKey: "usuarioId"
-});
 
 export default ModelAluno;
