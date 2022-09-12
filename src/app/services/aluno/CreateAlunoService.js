19663c7eb1a5d81f66c30c8f1e9fa08e4f6f0de5
Juliocoi <amorim.jc@gmail.com>
2022-09-11 21:55:33 -0300

    Alteração dos atributos segundo novo model

--- src/app/services/aluno/CreateAlunoService.js
+++ src/app/services/aluno/CreateAlunoService.js
@@ -8,7 +8,7 @@ class CreateAlunoService {
   async createAluno(nome, email, dataDeNascimento, mae, pai, matricula) {
     try {
       // o create abaixo é um metodo nativo do sequelize.
-      const newAluno = await ModelAluno.create({ id: v4(), nome, email, dataDeNascimento, mae, pai, matricula });
+      const newAluno = await ModelAluno.create({ id: v4(), matricula, nome, email, dataDeNascimento, mae, pai,});
 
       return newAluno;
 
