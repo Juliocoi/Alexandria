0ebee824d87aa4474d79fcf99241401050f1a87a
Juliocoi <amorim.jc@gmail.com>
2022-09-22 22:49:32 -0300

    atualizar emprestimo servive

--- src/app/services/emprestimo/AtualizarEmprestimoService.js
+++ src/app/services/emprestimo/AtualizarEmprestimoService.js
@@ -1,11 +1,13 @@
 import ModelLivro from '../../models/catalogo/ModelLivro';
-
 class AtualizarEmprestimoService {
     constructor(){}
     
     async emprestimo(id, disponivel){
         try{
+
         const livro = await ModelLivro.findByPk(id);
+
+            console.log(livro);
 
         if(!livro){
             return { mensagem: "Livro não registrado." }
@@ -15,14 +17,14 @@ class AtualizarEmprestimoService {
             { disponivel }, 
             { where: {id} }
 		);
+        
 
-        if (livroStatus === 0){
-                return { mensagem: "Este livro não está diponivel para emprestimo" };
-        } else {
-            return { 
-                mensagem:"Emprestimo realizado!" };
-        }       
-
+        // if (livro.getDataValue('disponivel') === 0){
+        //         return { mensagem: "Este livro não está diponivel para emprestimo" };
+        // } else {
+        // }       
+        return { disponivel };
+        
       } catch(error) {
         console.log(error);
         return { erro: error.message };
