66fe4ae361e5d9752aa780ab5b35c6fd7e6507ef
Lucas98s <lucasjls1998@gmail.com>
2022-09-25 18:57:31 -0300

    Middlewares de validação de campos

--- src/routes.js
+++ src/routes.js
@@ -24,6 +24,11 @@ import SessionAlunoController from './app/controllers/auth/SessionAlunoControlle
 import ValidarTokenSessao from './Middlewares/ValidarTokenSessao';
 import SessionFuncionarioController from './app/controllers/auth/SessionFuncionarioController';
 
+import alunoValidator from './Middlewares/AlunoValidator';
+import funcionarioValidator from './Middlewares/FuncionarioValidator'
+import livroValidator from './Middlewares/LivroValidator'
+import periodicoValidator from './Middlewares/PeriodicoValidator';
+
 const routes = new Router();
 
 const createAlunoController = new CreateAlunoController();
@@ -31,7 +36,7 @@ const listAllAlunoController = new ListAllAlunoController();
 const updateAlunoController = new UpdateAlunoController();
 const deleteAlunoController = new DeleteAlunoController();
 
-routes.post('/aluno', (req, resp) => createAlunoController.create(req, resp));
+routes.post('/aluno', alunoValidator, (req, resp) => createAlunoController.create(req, resp));
 routes.get('/alunos', (req, resp) => listAllAlunoController.index(req, resp));
 routes.get('/aluno/:nome', (req, resp) => listAllAlunoController.listarAlunosNome(req, resp));
 routes.put('/aluno/:matricula', (req, resp) => updateAlunoController.atualizar(req, resp));
@@ -42,7 +47,7 @@ const listAllFuncionarioController = new ListAllFuncionarioController();
 const updateFuncionarioController = new UpdateFuncionarioController();
 const deleteFuncionarioController = new DeleteFuncionarioController();
  
-routes.post('/funcionario', (req, resp) => createFuncionarioController.create(req, resp));
+routes.post('/funcionario', funcionarioValidator, (req, resp) => createFuncionarioController.create(req, resp));
 routes.get('/funcionarios', (req, resp) => listAllFuncionarioController.index(req, resp));
 routes.get('/funcionario/:nome', (req, resp) => listAllFuncionarioController.listarFuncionariosNome(req, resp));
 routes.put('/funcionario/:matricula', (req, resp) => updateFuncionarioController.update(req, resp));
@@ -53,7 +58,7 @@ const listLivroController = new ListLivroController();
 const updateLivroController = new UpdateLivroController();
 const deleteLivrosController = new DeleteLivrosController();
 
-routes.post('/livros', (req, resp) => createLivroController.create(req, resp));
+routes.post('/livros', livroValidator, (req, resp) => createLivroController.create(req, resp));
 routes.get('/livros', (req, resp) => listLivroController.index(req, resp));
 routes.get('/livro/:nome', (req, resp) => listLivroController.listarLivrosNome(req, resp));
 routes.put('/livro/:id', (req, resp) => updateLivroController.update(req, resp));
@@ -64,7 +69,7 @@ const listPeriodicoController = new ListPeriodicoController();
 const updatePeriodicoController = new UpdatePeriodicoController();
 const deletePeriodicoController = new DeletePeriodicoController();
 
-routes.post('/periodico', (req, resp) => createPeriodicoController.create(req, resp));
+routes.post('/periodico', periodicoValidator, (req, resp) => createPeriodicoController.create(req, resp));
 routes.get('/periodicos', (req, resp) => listPeriodicoController.index(req, resp));
 routes.get('/periodico/:nome', (req, resp)=> listPeriodicoController.listarPeriodicosNome(req, resp));
 routes.put('/periodico/:id', (req, resp) => updatePeriodicoController.update(req, resp));
